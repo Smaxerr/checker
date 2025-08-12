873dc62ef5bb11834bbaaf739112757b0d6c6633
Smaxerr <smax1027@icloud.com>
2025-08-12 18:22:17 +0100

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,7 @@
 # config.py
-BOT_TOKEN = "YOUR_TELEGRAM_BOT_TOKEN_HERE"
+BOT_TOKEN = "7580204485:AAE1f-PP9Fx4S2eEWxSLjd0C_-bgzFcWXBo"
 # Replace with your Telegram user IDs (integers). Only these IDs can run admin commands.
-ADMIN_IDS = [123456789]
+ADMIN_IDS = [8159560233]
 # Path to python executable to run subprocesses (optional) -- leave None to use sys.executable
 PYTHON_EXECUTABLE = None
+
