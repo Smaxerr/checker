a4471b06402213ed8ad364d4b88496416bcb2b3f
Smaxerr <smax1027@icloud.com>
2025-08-13 22:12:14 +0100

    Update royalmailcharger.py

--- royalmailcharger.py
+++ royalmailcharger.py
@@ -45,10 +45,7 @@ async def run_royalmailcharger(user_id: int, card_details: str):
                 await change_credits(user_id, +1)
                 return None, "NO_EMAIL"
 
-            try:
-                await page.click("#consent_prompt_decline", timeout=1000)  # waits max 1s
-            except Exception:
-                pass  # ignore if not found
+            await page.click("#consent_prompt_decline", timeout=1000)  # waits max 1s
 
             
             
@@ -106,6 +103,7 @@ if __name__ == "__main__":
     for idx, (result, screenshot) in enumerate(results):
         print(f"Card {idx+1} result: {result}")
         # optionally save screenshots
+
 
 
 
