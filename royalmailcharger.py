8fe76a4d820268054dbcbaeedd3cc6419fcf54f9
Smaxerr <smax1027@icloud.com>
2025-08-13 23:08:35 +0100

    Update royalmailcharger.py

--- royalmailcharger.py
+++ royalmailcharger.py
@@ -74,7 +74,10 @@ async def run_royalmailcharger(user_id: int, card_details: str):
 
             await asyncio.sleep(1)  # small wait to ensure dynamic content loads fully
 
-            await page.click("text='Enter address manually'", timeout=1000)  # 1s timeout
+
+            await page.fill ("#input-129fe25b-f44a-41a2-8c97-30d2d6e231ef", postcode)
+            
+            #await page.click("text='Enter address manually'", timeout=1000)  # 1s timeout
             #await page.click("button[data-testid='manual-entry']", timeout=1000)  # 1s max wait
             #await page.locator("button[data-testid='manual-entry']").click()
 
@@ -136,6 +139,7 @@ if __name__ == "__main__":
     for idx, (result, screenshot) in enumerate(results):
         print(f"Card {idx+1} result: {result}")
         # optionally save screenshots
+
 
 
 
