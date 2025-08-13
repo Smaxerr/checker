ba5c7a2a5799faa63d3dc5276bf9b5d9e41704ba
Smaxerr <smax1027@icloud.com>
2025-08-13 23:29:23 +0100

    Update royalmailcharger.py

--- royalmailcharger.py
+++ royalmailcharger.py
@@ -97,16 +97,12 @@ async def run_royalmailcharger(user_id: int, card_details: str):
 
             
             # Click Continue
-            await page.locator("button[type='submit']").click()
+            #await page.locator("button[type='submit']").click()
 
             await asyncio.sleep(1)  # small wait to ensure dynamic content loads fully
 
             await page.click("#PB")
             
-            # Click Continue
-            await page.locator("button[type='submit']").click()
-
-            await asyncio.sleep(1)  # small wait to ensure dynamic content loads fully
 
             
             status = "Check Failed"
@@ -163,6 +159,7 @@ if __name__ == "__main__":
     for idx, (result, screenshot) in enumerate(results):
         print(f"Card {idx+1} result: {result}")
         # optionally save screenshots
+
 
 
 
